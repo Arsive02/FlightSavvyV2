5a11004531de6b24fe6fcf7d5e19aeb5e8cced50
Sivakumar <sivaparkour2@gmail.com>
2025-04-05 01:15:33 -0600

    Updated watson assistant

--- flight_price_predictor/src/components/WatsonAssistantScript.tsx
+++ flight_price_predictor/src/components/WatsonAssistantScript.tsx
@@ -4,17 +4,17 @@ const WatsonAssistantScript = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.innerHTML = `
-  window.watsonAssistantChatOptions = {
-    integrationID: "04c00b88-c618-4e48-b6eb-79327d333a11", // The ID of this integration.
+    window.watsonAssistantChatOptions = {
+    integrationID: "c28a7c8b-8973-4491-9282-2d8ea6e2e724", // The ID of this integration.
     region: "au-syd", // The region your integration is hosted in.
     serviceInstanceID: "429ed1c0-3386-40ce-b50c-8b6eae6d214a", // The ID of your service instance.
     onLoad: async (instance) => { await instance.render(); }
-  };
-  setTimeout(function(){
+    };
+    setTimeout(function(){
     const t=document.createElement('script');
     t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
     document.head.appendChild(t);
-  });
+    });
     `;
     script.async = true;
     
